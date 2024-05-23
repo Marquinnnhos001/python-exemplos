3e4ca54f9cd2ef165833b99d63ee9210edff57cd
Marquinnnhos001 <marcosdecesarepalazan@gmail.com>
2024-05-23 10:50:28 -0300

    update

--- lista_comprimento.py
+++ lista_comprimento.py
@@ -1,4 +1,4 @@
-aluos=['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
+alunos=['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
 
 elementos = len(alunos)
 print(f'A lista alunos possui {elementos} elementos.')
