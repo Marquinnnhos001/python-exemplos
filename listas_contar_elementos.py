9bbab906fbc5cc59e4b961e8e9c020e743665f86
Marquinnnhos001 <marcosdecesarepalazan@gmail.com>
2024-05-23 10:36:04 -0300

    Create.listas_contar_elementos.py

---
+++ listas_contar_elementos.py
@@ -0,0 +1,5 @@
+alunos=['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
+
+ocorrencias = alunos.count('Adriana')
+print(f"Quantidade de ocorrencias de Adriana:
+{ocorrencias}")
